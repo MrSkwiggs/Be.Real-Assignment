05721c13257c96982d9fba0d8faa9d430bc77e61
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-11-16 23:20:07 +0100

    ✨ Create Root View Model

---
+++ Be.Folder/Scenes/Root/RootView+ViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  RootView+ViewModel.swift
+//  Be.Folder
+//
+//  Created by Dorian on 16/11/2022.
+//
+
+import Foundation
+import Combine
+import Core
+
+extension RootView {
+    class ViewModel: ObservableObject {
+        
+        private let loginProvider: LoginContract
+        
+        private var subscriptions: [AnyCancellable] = []
+        
+        @Published
+        var session: Session?
+        
+        init(loginProvider: LoginContract) {
+            self.loginProvider = loginProvider
+            
+            setupSubscriptions()
+        }
+        
+        private func setupSubscriptions() {
+            loginProvider
+                .sessionPublisher
+                .sink { [weak self] session in
+                    guard let self else { return }
+                    self.session = session
+                }
+                .store(in: &subscriptions)
+            
+        }
+    }
+}
