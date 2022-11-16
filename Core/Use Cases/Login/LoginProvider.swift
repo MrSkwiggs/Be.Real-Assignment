ed98855cc8e7642e93d403091b9c1226ce8c55cf
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-11-16 23:15:53 +0100

    ✨ Create Login Provider

---
+++ Core/Use Cases/Login/LoginProvider.swift
@@ -0,0 +1,54 @@
+//
+//  LoginProvider.swift
+//  Core
+//
+//  Created by Dorian on 16/11/2022.
+//
+
+import Foundation
+import Combine
+import Networking
+
+public class LoginProvider: LoginContract {
+    
+    private let sessionSubject: PassthroughSubject<Session, Never> = .init()
+    
+    public var sessionPublisher: AnyPublisher<Session, Never> { sessionSubject.eraseToAnyPublisher() }
+    
+    public func login(username: String, password: String) -> AnyPublisher<Bool, Login.Error> {
+        let publisher = PassthroughSubject<Bool, Login.Error>()
+        
+        guard username.contains(/:/) == false && password.contains(/:/) == false else {
+            publisher.send(.failure(.invalidCharacter))
+            return publisher.eraseToAnyPublisher()
+        }
+        
+        guard let token = "\(username):\(password)".data(using: .utf8)?.base64EncodedString() else {
+            publisher.send(.failure(.failedTokenEncoding))
+            return publisher.eraseToAnyPublisher()
+        }
+        BeFolderAPI().user(.me, token: token).perform { [weak self] result in
+            publisher.send(
+                result
+                    .mapError({ _ in .loginFailed })
+                    .map({
+                        self!.sessionSubject.send(.init(user: $0, token: token))
+                        return true
+                    })
+            )
+        }
+        
+        return publisher.eraseToAnyPublisher()
+    }
+}
+
+extension Subject {
+    func send(_ result: Result<Output, Failure>) {
+        switch result {
+        case .success(let success):
+            send(success)
+        case .failure(let failure):
+            send(completion: .failure(failure))
+        }
+    }
+}
