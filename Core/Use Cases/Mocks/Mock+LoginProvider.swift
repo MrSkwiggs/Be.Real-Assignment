a89c00eb0e02126362a453c036e79529f28748b7
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-11-16 23:16:22 +0100

    🤡 Mock Login Provider

---
+++ Core/Use Cases/Mocks/Mock+LoginProvider.swift
@@ -0,0 +1,41 @@
+//
+//  Mock+LoginProvider.swift
+//  Core
+//
+//  Created by Dorian on 16/11/2022.
+//
+
+import Foundation
+import Combine
+import Networking
+
+public extension Mock {
+    class LoginProvider: LoginContract {
+        
+        private let error: Login.Error?
+        private let sessionSubject: PassthroughSubject<Session, Never> = .init()
+        
+        /// Whether or not login attempts should succeed (fails with the given error, if any. Succeeds otherwise)
+        public init(error: Login.Error? = nil) {
+            self.error = error
+        }
+        
+        public lazy var sessionPublisher: AnyPublisher<Session, Never> = sessionSubject.eraseToAnyPublisher()
+        
+        public func login(username: String, password: String) -> AnyPublisher<Bool, Login.Error> {
+            let publisher = PassthroughSubject<Bool, Login.Error>()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+                guard let error = self.error else {
+                    publisher.send(true)
+                    self.sessionSubject.send(.init(user: Networking.Mock.user, token: "MuchS3cr3t"))
+                    return
+                }
+                
+                publisher.send(completion: .failure(error))
+            }
+            
+            return publisher.eraseToAnyPublisher()
+        }
+    }
+}
