6e615bab21a3810c7603831f59ee49ae7fc5dec2
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-11-16 23:21:19 +0100

    ♻️ Setup composed architecture

--- Be.Folder/SceneDelegate.swift
+++ Be.Folder/SceneDelegate.swift
@@ -12,9 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        let viewModelProvider = ViewModelProvider(root: .main())
+        
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: RootView())
+            let rootView = RootView(viewModel: viewModelProvider.rootViewModel)
+                .environmentObject(viewModelProvider)
+            window.rootViewController = UIHostingController(rootView: rootView)
             
             self.window = window
             window.makeKeyAndVisible()
